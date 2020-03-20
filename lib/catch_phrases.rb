c3069278a9f32de84fe2ef17e91a632dc59f6bad
Jordan Melidor-Fuxis <jordan.melidor@hotmail.fr>
2020-03-20 19:06:15 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -4,8 +4,7 @@ def mario
   puts phrase
 end
 
-status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-
 def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
